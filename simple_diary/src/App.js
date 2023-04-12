efb8389fa2e42b9e6939bb4ed0a21436b9c2450b
devum <doumeano0o@gmail.com>
2023-04-12 18:19:25 +0900

    2023.04.12 - API 호출해서 데이터 받아오기!

--- simple_diary/src/App.js
+++ simple_diary/src/App.js
@@ -1,11 +1,35 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
 function App() {
   const [data, setData] = useState([]);
+
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    // API에서 받아온 데이터로 초기 바인딩 데이터 생성
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1, // 1~5까지의 난수 생성 & 정수로 변환
+        created_date: new Date().getTime(),
+        id: dataId.current++, // dataId.current값을 id에 넣고 id 증가 시키기
+      }; // 배열의 객체 요소들을 API 값을 일기 데이터 구조로 변환
+    }); // 500개 배열에서 20개만 slice
+
+    setData(initData);
+  }; // Promise를 반환하는 비동기 함수
+
+  useEffect(() => {
+    getData();
+  }, []); // App 컴포넌트 Mount 시 호출
 
   // 일기를 추가하는 함수
   const onCreate = (author, content, emotion) => {
